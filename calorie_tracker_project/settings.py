818a812f10f387013b513345ad0d64f31a42e220
AlbertQuarshie <162796723+AlbertQuarshie@users.noreply.github.com>
2026-05-22 22:50:58 +0300

    Including the database configuration in the settings

--- calorie_tracker_project/settings.py
+++ calorie_tracker_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calorie_tracker',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'calorie_tracker_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'calorie_tracker',
+        'USER': 'postgres',
+        'PASSWORD': 'goldenwind',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
