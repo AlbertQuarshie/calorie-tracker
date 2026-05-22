f9ad7842299c345fc809bf5037983eb8dd5af7d0
AlbertQuarshie <162796723+AlbertQuarshie@users.noreply.github.com>
2026-05-22 22:51:55 +0300

    Adding the models into the app

--- calorie_tracker/models.py
+++ calorie_tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class FoodItem(models.Model):
+    name = models.CharField(max_length=200)
+    calories = models.IntegerField()
+    date_added = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name} - {self.calories} kcal"
