903930cb472385ec58997a1ac24768e8897e8e1e
AlbertQuarshie <162796723+AlbertQuarshie@users.noreply.github.com>
2026-05-23 21:59:58 +0300

    Fixing the delete item path in urls

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('delete/<int:item_id>/', views.views.delete_item, name='delete_item'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
     path('reset/', views.reset_today, name='reset_today'),
 ]
